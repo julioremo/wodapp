bbcbeedd1428a700daace5ffe82310e84764029b
julioremo <et.julio@gmail.com>
2026-03-18 16:06:40 +0100

    chore: reordered core packages

--- apps/web-athlete/src/lib/supabase.ts
+++ apps/web-athlete/src/lib/supabase.ts
@@ -1,8 +1,5 @@
-import { createWodClient } from '@wodapp/supabase-client';
-import { env } from '$env/dynamic/public';
+import { createWodappClient } from "@wodapp/supabase-client";
+import { env } from "$env/dynamic/public";
 
 // This is a singleton instance of the client
-export const supabase = createWodClient(
-  env.PUBLIC_SUPABASE_URL,
-  env.PUBLIC_SUPABASE_ANON_KEY
-);
+export const supabase = createWodappClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
--- packages/supabase-client/src/index.ts
+++ packages/supabase-client/src/index.ts
@@ -1,14 +1,14 @@
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from '@wodapp/types';
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "@wodapp/types";
 
 /**
- * We export a function to create the client. 
- * This allows us to pass different environment variables 
+ * We export a function to create the client.
+ * This allows us to pass different environment variables
  * depending on whether we are in the Athlete app or Admin app.
  */
-export const createWodClient = (supabaseUrl: string, supabaseKey: string) => {
+export const createWodappClient = (supabaseUrl: string, supabaseKey: string) => {
   return createClient<Database>(supabaseUrl, supabaseKey);
 };
 
 // Also export the types for convenience
-export * from '@supabase/supabase-js';
+export * from "@supabase/supabase-js";

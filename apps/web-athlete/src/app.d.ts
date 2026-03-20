7828d0305b4ebb33b6237f60b0c11a63856b208a
julioremo <et.julio@gmail.com>
2026-03-20 11:08:21 +0100

    chore: declare in locals the objects retrieved by hooks (supabase, user and location)

--- apps/web-athlete/src/app.d.ts
+++ apps/web-athlete/src/app.d.ts
@@ -1,13 +1,23 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
-}
+import type { Session, SupabaseClient, User } from "@supabase/supabase-js";
+import type { Database } from "@wodapp/types";
 
-export {};
+declare global {
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient<Database>;
+      safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
+      session: Session | null;
+      user: User | null;
+      location: {
+        id: string;
+        name: string;
+        slug: string;
+        settings: GymSettings | null;
+      } | null;
+      userRole: string | null;
+    }
+    interface PageData {
+      session: Session | null;
+    }
+  }
+}

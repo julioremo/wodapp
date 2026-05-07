2ec400b07c5ad318f9de292f827a8298141a9326
julioremo <et.julio@gmail.com>
2026-05-07 17:42:37 +0200

    Removed all aliases

--- apps/web-admin/svelte.config.js
+++ apps/web-admin/svelte.config.js
@@ -5,15 +5,14 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(),
-    alias: {
-      "@wodapp": path.resolve("../../")
-      // "@wodapp/types": path.resolve("../../packages/types/src/index.ts"),
-      // "@wodapp/core": path.resolve("../../packages/core/src/index.ts"),
-      // "@wodapp/supabase-client": path.resolve("../../packages/supabase-client/src/index.ts")
-    }
+    adapter: adapter()
+    // alias: {
+    //   "@wodapp": path.resolve("../../")
+    // "@wodapp/types": path.resolve("../../packages/types/src/index.ts"),
+    // "@wodapp/core": path.resolve("../../packages/core/src/index.ts"),
+    // "@wodapp/supabase-client": path.resolve("../../packages/supabase-client/src/index.ts")
+    // }
   }
 };
 

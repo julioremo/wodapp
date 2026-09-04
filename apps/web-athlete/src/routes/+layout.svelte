f32c426df03b6263cabcb97af4a4857ebfbe96c8
julioremo <et.julio@gmail.com>
2026-09-04 16:51:19 +0200

    fix: add webmanifest via layout

--- apps/web-athlete/src/routes/+layout.svelte
+++ apps/web-athlete/src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 import "../app.css";
+import { pwaInfo } from "virtual:pwa-info";
 import { useRegisterSW } from "virtual:pwa-register/svelte";
 import { Toaster } from "@ui/sonner";
 import { ModeWatcher } from "mode-watcher";
 import favicon from "$lib/assets/favicon.svg";
 
+const webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : "";
+
 const { needRefresh, updateServiceWorker } = useRegisterSW({
   onRegistered(r) {
-    if (r) {
-      r.update();
-    }
+    if (r) r.update();
   },
   onRegisterError(error) {
     console.error("Service worker registration failed:", error);
@@ -18,6 +19,8 @@ const { needRefresh, updateServiceWorker } = useRegisterSW({
 
 let { data, children } = $props();
 </script>
+
+<svelte:head>{@html webManifest}</svelte:head>
 
 <Toaster position="top-center" />
 <ModeWatcher defaultMode={data.theme} />
